64603b688586447e002e6c9d3d407cb8a036fd1d
elianaduarte <127699566+elianaduarte@users.noreply.github.com>
2023-03-12 21:29:21 -0600

    Add files via upload

---
+++ duarte_lab6.py
@@ -0,0 +1,24 @@
+# Eliana Duarte Lab 6
+
+def encode(password):
+    encpass = ''
+    for char in password:
+        char = int(char)+3
+        encpass = encpass + str(char)
+    return encpass
+
+quit = False
+menu = 'Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n'
+newpass = ''
+while not quit:
+    print(menu)
+    opt = int(input('Please enter an option: '))
+    if opt == 1:
+        password = input('Please enter your password to encode: ')
+        password = encode(password)
+        print('Your password has been encoded and stored!')
+    elif opt == 2:
+        newpass = decode(password)
+        print(f'The encoded password is {password}, and the original password is {newpass}.\n')
+    elif opt == 3:
+        quit = True
